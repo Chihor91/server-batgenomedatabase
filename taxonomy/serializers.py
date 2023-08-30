fe30a1a7cb2425bf405cb420b7ac6036d9790c09
ngarcia <ngarcia@up.edu.ph>
2023-08-31 00:06:36 +0800

    Add children to tax serializers

--- taxonomy/serializers.py
+++ taxonomy/serializers.py
@@ -9,38 +9,50 @@ from .models import (
     Genus, 
     Species
     )
+                
+class SpeciesSerializer(serializers.ModelSerializer):
+    parent = serializers.CharField(source='parent.name', read_only=True)
+    class Meta:
+        model = Species
+        fields = '__all__'
 
-class DomainSerializer(serializers.ModelSerializer):
+class GenusSerializer(serializers.ModelSerializer):
+    parent = serializers.CharField(source='parent.name', read_only=True)
+    species = SpeciesSerializer(many=True)
     class Meta:
-        model = Domain
+        model = Genus
         fields = '__all__'
-        
-class PhylumSerializer(serializers.ModelSerializer):
+
+class FamilySerializer(serializers.ModelSerializer):
+    parent = serializers.CharField(source='parent.name', read_only=True)
+    genera = GenusSerializer(many=True)
     class Meta:
-        model = Phylum
+        model = Family
         fields = '__all__'
-                
-class ClassSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Class
-        fields = '__all__'
-                
+
 class OrderSerializer(serializers.ModelSerializer):
+    parent = serializers.CharField(source='parent.name', read_only=True)
+    families = FamilySerializer(many=True)
     class Meta:
         model = Order
         fields = '__all__'
 
-class FamilySerializer(serializers.ModelSerializer):
+class ClassSerializer(serializers.ModelSerializer):
+    parent = serializers.CharField(source='parent.name', read_only=True)
+    orders = OrderSerializer(many=True)
     class Meta:
-        model = Family
+        model = Class
         fields = '__all__'
-        
-class GenusSerializer(serializers.ModelSerializer):
+
+class PhylumSerializer(serializers.ModelSerializer):
+    parent = serializers.CharField(source='parent.name', read_only=True)
+    classes = ClassSerializer(many=True)
     class Meta:
-        model = Genus
+        model = Phylum
         fields = '__all__'
-                
-class SpeciesSerializer(serializers.ModelSerializer):
+
+class DomainSerializer(serializers.ModelSerializer):
+    phyla = PhylumSerializer(many=True)
     class Meta:
-        model = Species
+        model = Domain
         fields = '__all__'
