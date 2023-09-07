8b00983e8e67e78c426ef5e58ad03188af37b531
ngarcia <ngarcia@up.edu.ph>
2023-09-08 02:12:08 +0800

    Change location model implementation

--- location/models.py
+++ location/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Location(models.Model):
-    location_name = models.CharField(max_length=255)
+    location_abbr = models.CharField(max_length=50)
+    location_province = models.CharField(max_length=255)
+    location_town = models.CharField(max_length=255)
+    location_long = models.FloatField(validators=[MinValueValidator(-180), MaxValueValidator(180)])
+    location_lat = models.FloatField(validators=[MinValueValidator(-90), MaxValueValidator(90)])
 
 class SamplingSite(models.Model):
+    site_abbr = models.CharField(max_length=50)
     site_name = models.CharField(max_length=255)
     location = models.ForeignKey(Location, related_name='sites', on_delete=models.CASCADE)
 
