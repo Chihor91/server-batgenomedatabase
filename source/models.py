ce2c159601a833e3e49e9b3385c520b090ba4d6f
ngarcia <ngarcia@up.edu.ph>
2023-09-09 02:12:43 +0800

    Add accession number to source model

--- source/models.py
+++ source/models.py
@@ -7,12 +7,14 @@ class Source(models.Model):
     source_institution = models.CharField(max_length=255)
     source_project = models.CharField(max_length=255)
     type_id = models.IntegerField(blank=True)
+    accession_no = models.CharField(max_length=255, blank=True)
 
 class Bacteria(Source):
     bacteria_id = models.AutoField(primary_key=True)
     def save(self, *args, **kwargs):
         super(Bacteria, self).save(*args, **kwargs)
         self.type_id = self.bacteria_id + 50000
+        self.accession_no = self.source_collection + '-' + self.source_institution + '-' + self.type_id
         super(Bacteria, self).save(*args, **kwargs)
 
 class Yeast(Source):
@@ -20,6 +22,7 @@ class Yeast(Source):
     def save(self, *args, **kwargs):
         super(Yeast, self).save(*args, **kwargs)
         self.type_id = self.yeast_id + 60000
+        self.accession_no = self.source_collection + '-' + self.source_institution + '-' + self.type_id
         super(Yeast, self).save(*args, **kwargs)
 
 class Mold(Source):
@@ -27,4 +30,5 @@ class Mold(Source):
     def save(self, *args, **kwargs):
         super(Mold, self).save(*args, **kwargs)
         self.type_id = self.mold_id + 70000
+        self.accession_no = self.source_collection + '-' + self.source_institution + '-' + self.type_id
         super(Mold, self).save(*args, **kwargs)
