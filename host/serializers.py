01cc256c5407210981dc4b0f2c8ea86b9658ee6c
ngarcia <ngarcia@up.edu.ph>
2023-09-19 01:05:55 +0800

    Remove taxonomy

--- host/serializers.py
+++ host/serializers.py
@@ -1,10 +1,8 @@
 from rest_framework import serializers
 
 from .models import Host
-from taxonomy.serializers import TaxonomySerializer
 
 class HostSerializer(serializers.ModelSerializer):
-    host_taxonomy = TaxonomySerializer(many = False, required = True)
     class Meta:
         model = Host
         fields = '__all__'
