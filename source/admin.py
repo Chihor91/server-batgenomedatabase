612a5f2b8c1338885d18900595dd2ed3898b686b
ngarcia <ngarcia@up.edu.ph>
2023-09-19 03:22:34 +0800

    Remove past implementation comments

--- source/admin.py
+++ source/admin.py
@@ -2,13 +2,7 @@ from django.contrib import admin
 from .models import (
     Project,
     Source,
-    # Bacteria,
-    # Yeast,
-    # Mold
 )
 # Register your models here.
 admin.site.register(Project)
 admin.site.register(Source)
-# admin.site.register(Bacteria)
-# admin.site.register(Yeast)
-# admin.site.register(Mold)
