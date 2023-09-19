76a0880ceb96dc8d2c8f7911140e98cc6929afd6
ngarcia <ngarcia@up.edu.ph>
2023-09-19 22:53:02 +0800

    Remove host

--- host/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import (
-    Host,
-    Sample
-)
-# Register your models here.
-
-admin.site.register(Host)
-admin.site.register(Sample)
--- host/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-from .models import Host
-
-class HostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Host
-        fields = '__all__'
