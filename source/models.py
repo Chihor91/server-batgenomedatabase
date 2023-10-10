5ab6e6d7f374afd075e251db1b19d9bc110ddd65
ngarcia <ngarcia@up.edu.ph>
2023-10-10 18:53:13 +0800

    change source and isolate relation

--- source/models.py
+++ source/models.py
@@ -15,14 +15,10 @@ class Project(models.Model):
 class Source(models.Model):
     collection = models.CharField(max_length=25)
     institution = models.CharField(max_length=50)
-    type = models.IntegerField()    # 1 - Bacteria, 2 = Yeast, 3 = Mold
-    type_id = models.IntegerField(blank=True)
     
     host_type = models.CharField(max_length=25, blank=True) 
     host_species = models.CharField(max_length=50, blank=True)
     sample_type = models.CharField(max_length=25)
-
-    accession_no = models.CharField(max_length=100, blank=True)
 
     human_readable_id = models.CharField(max_length=150, blank=True)
 
@@ -31,21 +27,26 @@ class Source(models.Model):
 
 
     def save(self, *args, **kwargs):
-        self.type_id = (self.type + 4) * 10000 + (Source.objects.filter(type=self.type).count() + 1)
-        self.accession_no = '-'.join([self.collection, self.institution, str(self.type_id)]) 
-        self.human_readable_id = '-'.join([self.collection, self.institution, self.project.abbr, self.sampling_point.cave.location.abbr, self.sampling_point.cave.abbr, str(self.sampling_point.point_number), str(self.type), self.host_type, self.sample_type])
+        self.human_readable_id = '-'.join([self.collection, self.institution, self.project.abbr, self.sampling_point.cave.location.abbr, self.sampling_point.cave.abbr, str(self.sampling_point.point_number), self.host_type, self.sample_type])
         super(Source, self).save(*args, **kwargs)
 
     def __str__(self):
-        return self.accession_no
+        return self.human_readable_id
     
 class Isolate(models.Model):
+    source = models.ForeignKey(Source, related_name='isolates', on_delete=models.CASCADE)
     human_readable_id = models.CharField(max_length=150, blank=True)
-    source = models.ForeignKey(Source, on_delete=models.CASCADE)
+    accession_no = models.CharField(max_length=100, blank=True)
+    type = models.IntegerField()    # 1 - Bacteria, 2 = Yeast, 3 = Mold
+    type_id = models.IntegerField(blank=True)
+
 
     def save(self, *args, **kwargs):
-        super(Isolate, self).save(*args, **kwargs)
-        self.human_readable_id = '-'.join([self.source.human_readable_id, str(self.id).zfill(5)])
+        if not self.type_id:
+            self.type_id = self.source.isolates.count() + 1
+        
+        self.human_readable_id = '-'.join([self.source.human_readable_id, str(self.type_id).zfill(3)])
+        self.accession_no = '-'.join([self.source.collection, self.source.institution, str((4 + self.type) * 10000 + self.type_id)])
         super(Isolate, self).save(*args, **kwargs)
 
     def __str__(self):
