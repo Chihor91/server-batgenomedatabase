a10ed7e36cc59a08505c29a9f5e61fdbb58c1f55
ngarcia <ngarcia@up.edu.ph>
2024-03-09 03:32:20 +0800

    fix bacdive login logic

--- bacdiveapi/views.py
+++ bacdiveapi/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
-from django.conf import settings
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 import bacdive
 
-client = bacdive.BacdiveClient('ngarcia@up.edu.ph', 'popowhee123')
+# client = bacdive.BacdiveClient('ngarcia@up.edu.ph', 'popowhee123')
 
 # Create your views here.
 class TestView(APIView):
     def get(self, request, format=None):
         strains = []
+        client = bacdive.BacdiveClient('ngarcia@up.edu.ph', 'popowhee123')
         client.setSearchType('exact')
-        query = {"genome": ["GCA_003332855", "GCA_024623325", "GCA_017377855"]}
+        # query = {"taxonomy": ("Peptostreptococcus", "anaerobius")}
+        query = {"id": 11843}
         client.search(**query)
         
         for strain in client.retrieve():
