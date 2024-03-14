0808f71e37aa23af20f3381b37dccf4a088cbdec
ngarcia <ngarcia@up.edu.ph>
2024-03-15 02:07:20 +0800

    add initial multi add isolates

--- source/views.py
+++ source/views.py
@@ -1,17 +1,24 @@
 from rest_framework import viewsets, status
+from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from django_filters.rest_framework import DjangoFilterBackend
+import csv
+import codecs
 
 # Create your views here.
 from .serializers import (
     SourceSerializer,
-    StrainSerializer
+    IsolateSerializer
 )
 
 from .models import (
     Source,
-    Strain
+    Isolate
 )
+
+from user.models import Account
+from django.db.models import Q 
 
 class SourceViewSet(viewsets.ModelViewSet):
     queryset = Source.objects.all()
@@ -29,18 +36,49 @@ class SourceViewSet(viewsets.ModelViewSet):
                 status = status.HTTP_500_INTERNAL_SERVER_ERROR
             )
 
-class StrainViewSet(viewsets.ModelViewSet):
-    queryset = Strain.objects.all()
+class IsolateViewSet(viewsets.ModelViewSet):
+    parser_classes = [MultiPartParser, FormParser]
+    queryset = Isolate.objects.all()
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['id','source']
 
-    serializer_class = StrainSerializer
+    serializer_class = IsolateSerializer
 
     def create(self, request, *args, **kwargs):
         try:
-            return super().create(request, *args, **kwargs)
-        except:
+            if Account.objects.filter(id=request.user.id).exists():
+                request.data["author_id"] = request.user.id
+                request.data["author"] = request.user.username
+                return super().create(request, *args, **kwargs)
+            else:   return Response(
+                {'error': 'User not authenticated.'},
+                status = status.HTTP_401_UNAUTHORIZED
+            )
+        except Exception as e:
+            print(e)
             return Response(
-                {'message': 'An error has occurred while creating isolate.'},
+                {'message': 'An error has occurred while adding isolate.'},
                 status = status.HTTP_500_INTERNAL_SERVER_ERROR
             )
+        
+    @action(detail=False, methods=['POST'], name='Import from file')
+    def import_from_file(self, request, *args, **kwargs):
+        file = request.data['isolates']
+        reader = csv.DictReader(codecs.iterdecode(file, 'utf-8'))
+        for row in reader:
+            print(row['SOURCE'])
+        return(Response(status = status.HTTP_200_OK))
+        
+    def list(self, request):
+        user = request.user
+        print(user)
+        if user.is_superuser:
+            isolates = Isolate.objects.all()
+        elif not user.is_anonymous:
+            print("Authenticated")
+            isolates = Isolate.objects.filter(Q(visibility="Public") | Q(visibility="Researchers Only") | Q(author_id = user.id))
+        else:
+            isolates = Isolate.objects.filter(visibility="Public")
+
+        serializer = self.get_serializer(isolates, many=True)
+        return Response(serializer.data)
