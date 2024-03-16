5559cd6b81478c762e389fa13e688878e9fc7657
ngarcia <ngarcia@up.edu.ph>
2024-03-16 14:33:39 +0800

    revise source and isolate urls

--- source/urls.py
+++ source/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-
+from django.urls import path, re_path
 from .views import (
     SourceViewSet,
     IsolateViewSet
@@ -10,4 +10,14 @@ router = DefaultRouter()
 router.register('source', SourceViewSet)
 router.register('isolate', IsolateViewSet)
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('view/all/', SourceViewSet.as_view({'get': 'list'})),
+    path('view/id/<int:pk>/', SourceViewSet.as_view({'get': 'retrieve'})),
+    path('add/', SourceViewSet.as_view({'post': 'create'})),
+
+    path('isolate/view/all/', IsolateViewSet.as_view({'get': 'list'})),
+    path('isolate/view/id/<int:pk>/', IsolateViewSet.as_view({'get': 'retrieve'})),
+    path('isolate/source/<int:pk>/', IsolateViewSet.as_view({'get': 'get_by_id'})),
+    path('isolate/add/', IsolateViewSet.as_view({'post': 'create'})),
+
+]
--- source/views.py
+++ source/views.py
@@ -1,7 +1,10 @@
 from rest_framework import viewsets, status
-from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.parsers import MultiPartParser, FormParser, JSONParser
 from rest_framework.response import Response
 from rest_framework.decorators import action
+
+
+from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 import csv
 import codecs
@@ -22,8 +25,6 @@ from django.db.models import Q
 
 class SourceViewSet(viewsets.ModelViewSet):
     queryset = Source.objects.all()
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['id']
 
     serializer_class = SourceSerializer
 
@@ -36,11 +37,16 @@ class SourceViewSet(viewsets.ModelViewSet):
                 status = status.HTTP_500_INTERNAL_SERVER_ERROR
             )
 
+    def retrieve(self, request, pk=None):
+        print(pk)
+        source = get_object_or_404(self.get_queryset(), pk=pk)
+        serializer = self.get_serializer(source)
+        
+        return Response(serializer.data)
+
 class IsolateViewSet(viewsets.ModelViewSet):
-    parser_classes = [MultiPartParser, FormParser]
+    parser_classes = [MultiPartParser, FormParser, JSONParser]
     queryset = Isolate.objects.all()
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['id','source']
 
     serializer_class = IsolateSerializer
 
@@ -68,14 +74,47 @@ class IsolateViewSet(viewsets.ModelViewSet):
         for row in reader:
             print(row['SOURCE'])
         return(Response(status = status.HTTP_200_OK))
+    
+    @action(detail=True, methods=['get'], name='Get by ID')
+    def get_by_id(self, request, pk=None):
+        try:
+            user = request.user
+            if user.is_superuser:
+                isolates = Isolate.objects.filter(source=pk)
+            elif not user.is_anonymous:
+                isolates = Isolate.objects.filter(Q(visibility="Public") | Q(visibility="Researchers Only") | Q(author_id = user.id), source=pk)
+            else:
+                isolates = Isolate.objects.filter(source=pk, visibility="Public")
+            serializer = self.get_serializer(isolates, many=True)
+            return Response(serializer.data)
+        except Exception as e:
+            print(e)
+            return Response(
+                {'message': 'An error has occurred while adding isolate.'},
+                status = status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
         
+    def retrieve(self, request, pk=None):
+        user = request.user
+        isolate = get_object_or_404(self.get_queryset(), pk=pk)
+        serializer = self.get_serializer(isolate)
+
+        if (user.is_superuser 
+            or serializer.data['author_id'] == user.id 
+            or serializer.data['visibility'] == "Public" 
+            or (serializer.data['visibility'] == "Researchers Only" 
+                and not user.is_anonymous)):
+            return Response(serializer.data)
+        else: return Response(
+            {'error': 'Isolate not found.'},
+            status= status.HTTP_404_NOT_FOUND
+        )
+
     def list(self, request):
         user = request.user
-        print(user)
         if user.is_superuser:
             isolates = Isolate.objects.all()
         elif not user.is_anonymous:
-            print("Authenticated")
             isolates = Isolate.objects.filter(Q(visibility="Public") | Q(visibility="Researchers Only") | Q(author_id = user.id))
         else:
             isolates = Isolate.objects.filter(visibility="Public")
